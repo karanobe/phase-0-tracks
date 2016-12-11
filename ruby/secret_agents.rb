4138332b4e1f8ffad2c882ff27c1232db3fedadc
Karan Oberoi <koberoi1017@gmail.com>
2016-12-11 12:20:51 -0800

    Decrypt edge cases; pseudocode

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,27 +1,40 @@
+#An Encryptor
+#INPUT: a word or phrase - String
 def encrypt(str)
-  index = 0
-    while index < str.length
-      print str[index].next
-      index += 1
-    end
+#OUTPUT: each letter in the input 
+ index = 0
+   while index < str.length
+#switched to the letter following it, alphabetically - String
+   print str[index].next
+   index += 1
+   end
 end
+  #Example Input: "hello"
+  #Example Output: "ifmmp"
 
 
+
+#A Decryptor
+#INPUT: a word or phrase - String
 def decrypt(str)
-  index=0
-    while index < str.length
-      print (str[index].ord-1).chr
-      index += 1  
-    end
+ 
+ index=0
+#OUTPUT: each letter in the input  
+ while index < str.length
+#switched to the letter preceding it, alphabetically - String
+   output = (str[index].ord-1).chr
+   print output.tr("`", "z")
+   index += 1  
+ end
+#Example Input: "hello"
+#Example Output: "gdkkm"
+ 
 end
-  
+ 
 puts encrypt("abc")
 puts encrypt("zed")
 puts decrypt("bcd")
 puts decrypt("afe")
 puts encrypt("swordfish")
 puts decrypt("txpsegjti")
- 
-
- 
-
+puts decrypt(encrypt("swordfish"))
