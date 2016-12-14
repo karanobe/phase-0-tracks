524f8ebb141e424f4fa8f8dfc7f2dd078629e261
Karan Oberoi <koberoi1017@gmail.com>
2016-12-13 21:33:14 -0800

    Print all

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,37 +4,38 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-"iNvEsTiGaTiOn".swapcase
+p "iNvEsTiGaTiOn".swapcase
+ 
 # => “InVeStIgAtIoN”
 
-"zom".insert(2, 'o')
+p "zom".insert(2, 'o')
 # => “zoom”
 
-"enhance".center(17)
+p "enhance".center(17)
 # => "    enhance    "
 
-"Stop! You’re under arrest!".upcase
+p "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-"the usual".insert(9, " suspects")
+p "the usual".insert(9, " suspects")
 #=> "the usual suspects"
 
-" suspects".replace("the usual suspects")
+p " suspects".replace("the usual suspects")
 # => "the usual suspects"
 
-"The case of the disappearing last letter".chop
+p "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-"The mystery of the missing first letter".delete "T"
+p "The mystery of the missing first letter".delete "T"
 # => "he mystery of the missing first letter"
 
-"Elementary,    my   dear        Watson!".squeeze(" ")
+p "Elementary,    my   dear        Watson!".squeeze(" ")
 # => "Elementary, my dear Watson!"
 
-"z".ord
+p "z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 #Return the integer ordinal of a one-character string.
 
-"How many times does the letter 'a' appear in this string?".count(?a)
+p "How many times does the letter 'a' appear in this string?".count(?a)
 # => 4
