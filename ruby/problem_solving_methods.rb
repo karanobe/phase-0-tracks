f59d16a621fdc786fdb76dc51ac7207819d6d927
Karan Oberoi <koberoi1017@gmail.com>
2016-12-18 00:33:59 -0800

    problem_solving_methods.rb

---
+++ ruby/problem_solving_methods.rb
@@ -0,0 +1,38 @@
+#RELEASE 0
+#Write a method that takes an array of integers
+arr = [1,2,3,4,5,6,7,8,9,10]
+
+def search_array(array, indivual)
+  index = 0
+  while index < array.length
+    if array[index] == indivual
+      p index
+    end
+  index += 1
+  end
+end
+
+#and an integer to search for
+p search_array(arr, 2)
+p search_array(arr, 21)
+
+#The method should return the index of the item, or nil if the item is not present in the array
+
+
+#RELEASE 1
+[a,b]
+
+while 
+index = 0
+0 + 1 = 1
+a + b = c
+b = a
+c = b
+index += 1
+end
+
+1 + 1 = 2
+1+ 2 = 3
+2 + 3 = 5
+
+fib(100)
