ea031cf6c106069af63326dde29fd5418ffe8b4a
Karan Oberoi <koberoi1017@gmail.com>
2016-12-22 10:23:52 -0800

    Pseudocode, GPS 2.2

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,33 @@
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+  # prompt the user to enter in a list; type in a grocery item
+  # puts statement "What would you like to add to your grocery list?"
+    # user_input_food = gets.chomp
+    # user_input_food will be split on the " " and entered in as the KEY in the hash
+    # default quantity could be 1 for value in hash
+    # User will be continue to enter in items, DO loop; until they type in 'done'
+  # set default quantity
+  # print the list to the console [can you use one of your other methods here?]
+# output: [what data type goes here, array or hash?] - Hash
+
+# Method to add an item to a list
+# input: list, item name, and optional quantity
+# steps: take the user_input_food and STORE the variable into a hash
+        # use the .store built in method to add 
+# output: updated hash 
+
+# Method to remove an item from the list
+# input: build in delete 
+# steps:
+# output:
+
+# Method to update the quantity of an item
+# input:
+# steps:
+# output:
+
+# Method to print a list and make it look pretty
+# input:
+# steps:
+# output:
