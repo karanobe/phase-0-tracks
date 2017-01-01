28207b4e5e069edbc11da204d586c440673daa8f
Karan Oberoi <koberoi1017@gmail.com>
2016-12-31 16:49:08 -0800

    Mixin Modules

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,4 +1,15 @@
 module Shout
-  # we'll put some methods here soon, but this code is fine for now!
+  def self.yell_angrily(words)
+      words + "!!!" + " :("
+  end
+
+  def self.yell_happily(words)
+      words.upcase + "!!!!" + " :)"
+  end
+
 end
+
+words = "you're pregnant?"
+p Shout.yell_angrily(words)
+p Shout.yell_happily(words)
 
