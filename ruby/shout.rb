d39a2d80176938da3fd96e0a4fa4a66eabc11145
Karan Oberoi <koberoi1017@gmail.com>
2016-12-31 16:57:39 -0800

    Release 3: Standalone Module to a Mixin

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,15 +1,44 @@
+#module Shout
+#  def self.yell_angrily(words)
+#      words + "!!!" + " :("
+#  end
+#
+#  def self.yell_happily(words)
+#      words.upcase + "!!!!" + " :)"
+#  end
+#
+#end
+#
+#words = "you're pregnant?"
+#p Shout.yell_angrily(words)
+#p Shout.yell_happily(words)
+
 module Shout
-  def self.yell_angrily(words)
-      words + "!!!" + " :("
+
+  def scream_scared(words)
+    words.upcase + "!!!!!!! OH MY GOD!!!!!"
   end
 
-  def self.yell_happily(words)
-      words.upcase + "!!!!" + " :)"
+  def scream_crazy(crazy_words)
+    crazy_words.upcase + "HA! HA! HA! HA!"
   end
 
 end
 
-words = "you're pregnant?"
-p Shout.yell_angrily(words)
-p Shout.yell_happily(words)
+class Damsel_in_distress
+  include Shout
+end
+
+class Villain
+  include Shout
+end
+
+spidermans_girlfriend = Damsel_in_distress.new
+p spidermans_girlfriend.scream_scared("aaaahhhhhh")
+
+doctor_octavious = Villain.new
+p doctor_octavious.scream_crazy("you're dead spiderman")
+
+  
+
 
