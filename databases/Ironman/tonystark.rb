4a453ae61eb4e5530c7e4883a04f45ec21cff8bb
Karan Oberoi <koberoi1017@gmail.com>
2017-01-15 12:41:44 -0800

    Initial commit: require sqlite/faker

---
+++ databases/Ironman/tonystark.rb
@@ -0,0 +1,24 @@
+# OPERATION IRONSUIT EXPLOSION
+
+# talk about Object Relational Mapping (ORM)
+
+# require gems
+require 'sqlite3'
+require 'faker'
+
+# create SQLite3 database
+# sqlite3 somedatabase.db - from terminal 
+db = SQLite3::Database.new("ironman_suits.db")
+
+
+# learn about fancy string delimiters
+
+# create a Ironman Suit table (if it's not there already)
+
+# add a test suit
+
+# explore ORM by retrieving data
+
+# add LOOOOTS of suits!
+# so. many. suits.
+# SuitExplosion
