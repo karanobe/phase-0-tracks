d7d2204fcfcc65ebe1da3dbc8bca5be17ee8cebf
Karan Oberoi <koberoi1017@gmail.com>
2017-01-22 15:44:15 -0600

    Jquery commit 9.2/9.3

--- web_dev/client_side_js/9.2_pairing_session.js
+++ web_dev/client_side_js/9.2_pairing_session.js
@@ -1,9 +1,21 @@
 console.log("The script is running!");
 
-function addRedBorder(event) {
+function catsAreCool(event) {
   console.log("You clicked!");
-  event.target.style.border = "15px solid red";
+  event.target.style.border = "3px solid red";
 }
 
-var variable = document.getElementById("id");
-variable.addEventListener("click", addRedBorder);//
+var variable = document.getElementById("page");
+variable.addEventListener("click", catsAreCool);//
+
+
+function addListItem(event) {
+  console.log("you have added a list item!!!");
+  var node = document.createElement("LI");
+  var textnode = document.createElement("test");
+  node.appendChild(textnode);
+  document.getElementById("list").appendChild(node);
+}
+
+  var ul = document.getElementById("list");
+  ul.addEventListener("click", addListItem);
---
+++ web_dev/jquery/notes.md
@@ -0,0 +1,5 @@
+#run through the jquery course from our one month course to refresh our memory on jquery.
+#research the greater advantages and possiblilties of jquery
+#make a fun interactive page with jquery
+#make it look cute
+#make a simple blankish webpage that will be built up of jquery and javascript prompts that ask user for inputs and changes the page accordingly
